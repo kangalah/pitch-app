5a24b5e8bfaac1ddb23c6cc152afb1b48160d557
kangalah <nyongesajoannanjala@gmail.com>
2020-07-13 22:12:18 +0300

    import migrate and migratecommand from the flask migrate

--- manage.py
+++ manage.py
@@ -1,29 +1,34 @@
-from app import create_app
-# from app.models import User,Role,Review
-from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User, Pitch, Comments, PitchCategory
 
-#Creating app instance
+from flask_migrate import Migrate, MigrateCommand
+
+# Creating app instance
 app = create_app('development')
-
+# app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
-# manager.add_command('server',Server)
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
 
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app,db = db,User = User, Role = Role )
+@manager.command
+def test():
+    """
+    Run unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch = Pitch)
+    # pass
+
 
 if __name__ == '__main__':
     manager.run()
